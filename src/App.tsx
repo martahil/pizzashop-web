5e30c6ba0c17d88ed0fafe05f8d822f8e0f66c45
Marta Hilgert <marta.hil.1988@gmail.com>
2025-04-21 18:24:54 -0300

    Feat(seo): integrate react-helmet-async for dynamic page titles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import { RouterProvider } from 'react-router-dom'
 import '../index.css'
 import { router } from './routes'
+import { HelmetProvider, Helmet } from 'react-helmet-async'
 
 export function App() {
 
   return (
-    <RouterProvider router={router} />
+    <HelmetProvider>
+      <Helmet titleTemplate='%s | pizza.shop' />
+      <RouterProvider router={router} />
+    </HelmetProvider>
   )
 }
