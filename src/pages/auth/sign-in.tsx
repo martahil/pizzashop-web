6f314ed9887fc61823abcac174441294714c8271
Marta Hilgert <marta.hil.1988@gmail.com>
2025-04-22 20:18:27 -0300

    Add sign-up route and form for restaurant registration

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -5,7 +5,7 @@ import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
 import { toast } from 'sonner'
-import { error } from 'console'
+import { Link } from 'react-router-dom'
 
 const signInForm = z.object({
   email: z.string().email(),
@@ -35,6 +35,12 @@ export function SignIn() {
     <>
       <Helmet title='Login' />
       <div className='p-8'>
+        <Button variant='ghost' asChild className='absolute right-8 top-8'>
+          <Link to='/sign-up' className=''>
+            New establishment
+          </Link>
+        </Button>
+
         <div className='w-[350px] flex flex-col justify-center gap-6'>
           <div className='flex flex-col gap-2 text-center'>
             <h1 className='text-2xl font-semibold tracking-tight'>Access Dashboard</h1>
