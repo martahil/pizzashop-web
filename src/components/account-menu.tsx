5e56a758b824380ffa0a2e7c6ede1d3a6268dbd4
Marta Hilgert <marta.hil.1988@gmail.com>
2025-05-02 16:58:53 -0300

    Feat: fetch and display user and restaurant data in account menu

--- src/components/account-menu.tsx
+++ src/components/account-menu.tsx
@@ -1,20 +1,33 @@
 import { Building, ChevronDown, LogOut } from "lucide-react";
 import { Button } from "./ui/button";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "./ui/dropdown-menu";
+import { useQuery } from "@tanstack/react-query";
+import { getProfile } from "@/api/get-profile";
+import { getManagedRestaurant } from "@/api/get-managed-restaurant";
 
 export function AccountMenu() {
+  const { data: profile } = useQuery({
+    queryKey: ['profile'],    
+    queryFn: getProfile,
+  })
+
+  const { data: managedRestaurant } = useQuery({
+    queryKey: ['managed-restaurant'],    
+    queryFn: getManagedRestaurant,
+  })
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant='outline' className="flex items-center gap-2 select-none">
-          Pizza Shop
+          {managedRestaurant?.name}
           <ChevronDown className="w-4 h-4" /> 
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end" className="w-56">
         <DropdownMenuLabel className="flex flex-col">
-          <span>Marta Hil</span>
-          <span className="text-xs font-normal text-muted-foreground">user.email@gmail.com</span>
+          <span>{profile?.name}</span>
+          <span className="text-xs font-normal text-muted-foreground">{profile?.email}</span>
         </DropdownMenuLabel>
         <DropdownMenuSeparator />
         <DropdownMenuItem>
