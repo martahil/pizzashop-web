e3e3823a9360d2e77adaf148790f3d26d6a83223
Marta Hilgert <marta.hil.1988@gmail.com>
2025-05-15 22:11:11 -0300

    Test: add Playwright tests for authentication and sign-up navigation

---
+++ test/sign-in.e2e-spec.ts
@@ -0,0 +1,41 @@
+import { test, expect } from '@playwright/test'
+
+test('sign in successfully', async ({ page }) => {
+  await page.goto('/sign-in', { waitUntil: 'networkidle' })
+
+  await page.getByRole('textbox', { name: 'Email address' }).fill('johndoe@example.com')
+  await page.getByRole('button', { name: 'Access Dashboard' }).click()
+
+  const toast = page.getByText(
+    'An authentication link has been sent to your email address.'
+  )
+
+  expect(toast).toBeVisible()
+
+  //await page.waitForTimeout(2000)
+})
+
+test('sign in with wrong credentials', async ({ page }) => {
+  await page.goto('/sign-in', { waitUntil: 'networkidle' })
+
+  await page.getByRole('textbox', { name: 'Email address' }).fill('wrong@example.com')
+  await page.getByRole('button', { name: 'Access Dashboard' }).click()
+
+  const toast = page.getByText(
+    'Invalid credentials.'
+  )
+
+  expect(toast).toBeVisible()
+
+  await page.waitForTimeout(2000)
+})
+
+test('navigate to new restaurant page', async ({ page }) => {
+  await page.goto('/sign-in', { waitUntil: 'networkidle' })
+
+  await page.getByRole('link', { name: 'New establishment' }).click()
+
+  expect(page.url()).toContain('/sign-up')
+})
+
+
